996310fcef22934ad4f5c575d3312c501200d49b
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-05-20 17:16:49 +0200

    Fix

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,11 @@
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
+import UserMenu from 'components/UserMenu/UserMenu';
 
 const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <AppBar position="fixed" color="secondary">
       <Toolbar>
@@ -13,30 +17,34 @@ const Navigation = () => {
         >
           Home
         </Typography>
-        <Box
-          sx={{
-            minWidth: 200,
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Button
-            variant="outlined"
-            color="primary"
-            component={Link}
-            to="/login"
+        {isLoggedIn ? (
+          <UserMenu />
+        ) : (
+          <Box
+            sx={{
+              minWidth: 200,
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
           >
-            Login
-          </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            component={Link}
-            to="/register"
-          >
-            Register
-          </Button>
-        </Box>
+            <Button
+              variant="outlined"
+              color="primary"
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              component={Link}
+              to="/register"
+            >
+              Register
+            </Button>
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
