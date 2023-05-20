f56de732a81827c16f623c197263a77843c96e1a
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-05-20 13:26:26 +0200

    Added UserMenu & Contacts page

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,21 @@
+import { useAuth } from 'hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/authOperations';
+import { Button } from '@mui/material';
+
+const UserMenu = () => {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+  return (
+    <div>
+      Welcome {user.name}
+      <Button
+        type="button"
+        variant="outlined"
+        onClick={() => dispatch(logOut())}
+      ></Button>
+    </div>
+  );
+};
+
+export default UserMenu;
